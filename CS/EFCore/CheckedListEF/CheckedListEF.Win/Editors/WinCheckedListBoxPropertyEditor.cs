9c018bbd18a75282aecdb67fb480db88267a5d87
andrey <andrey.kozhevnikov@devexpress.com>
2023-03-15 19:10:36 +0400

    make win editor more generic

--- CS/EFCore/CheckedListEF/CheckedListEF.Win/Editors/WinCheckedListBoxPropertyEditor.cs
+++ CS/EFCore/CheckedListEF/CheckedListEF.Win/Editors/WinCheckedListBoxPropertyEditor.cs
@@ -7,34 +7,38 @@ using System.Windows.Forms;
 using DevExpress.ExpressApp.Editors;
 using DevExpress.ExpressApp.Model;
 using CheckedListEF.Module;
+using DevExpress.Persistent.BaseImpl.EF;
+using System.Collections.ObjectModel;
+using System.Collections;
 
 namespace CheckedList.Module.Win {
-    [PropertyEditor(typeof(IList<Detail>), false)]
+    [PropertyEditor(typeof(IList<Object>), false)]
     public class WinCheckedListBoxPropertyEditor : WinPropertyEditor, IComplexViewItem {
         public WinCheckedListBoxPropertyEditor(Type objectType, IModelMemberViewItem model) : base(objectType, model) { }
         protected override object CreateControlCore() {
             return new CheckedListBoxControl();
         }
-        IList<Detail> checkedItems;
+        IList checkedItems;
         XafApplication application;
         IObjectSpace objectSpace;
+
         protected override void ReadValueCore() {
             base.ReadValueCore();
-            if (PropertyValue is IList<Detail>) {
-                Control.ItemCheck -= new DevExpress.XtraEditors.Controls.ItemCheckEventHandler(control_ItemCheck);
-                checkedItems = (IList<Detail>)PropertyValue;
-                IList<Detail> dataSource = objectSpace.GetObjects<Detail>(); 
+            Control.ItemCheck -= new DevExpress.XtraEditors.Controls.ItemCheckEventHandler(control_ItemCheck);
+            checkedItems = (IList)PropertyValue;
+            List<Object> dataSource = objectSpace.GetObjects(MemberInfo.ListElementTypeInfo.Type).Cast<Object>().ToList();
 
-                Control.DataSource = dataSource;
-                Control.DisplayMember = nameof(Detail.DetailName);
-                foreach (Detail obj in checkedItems) {
-                    Control.SetItemChecked(dataSource.IndexOf(obj), true);
-                }
-                Control.ItemCheck += new DevExpress.XtraEditors.Controls.ItemCheckEventHandler(control_ItemCheck);
+            Control.DataSource = dataSource;
+            IModelClass classInfo = application.Model.BOModel.GetClass(MemberInfo.ListElementTypeInfo.Type);
+            Control.DisplayMember = classInfo.DefaultProperty;
+            foreach (object obj in checkedItems) {
+                Control.SetItemChecked(dataSource.IndexOf(obj), true);
             }
+            Control.ItemCheck += new DevExpress.XtraEditors.Controls.ItemCheckEventHandler(control_ItemCheck);
+            //   }
         }
         void control_ItemCheck(object sender, DevExpress.XtraEditors.Controls.ItemCheckEventArgs e) {
-            Detail obj = (Detail)Control.GetItemValue(e.Index);
+            object obj = Control.GetItemValue(e.Index);
             switch (e.State) {
                 case CheckState.Checked:
                     checkedItems.Add(obj);
