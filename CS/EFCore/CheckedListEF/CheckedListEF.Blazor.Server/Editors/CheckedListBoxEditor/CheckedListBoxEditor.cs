eecb72b1e2641a115d57eff9a194893fd354be20
andrey <andrey.kozhevnikov@devexpress.com>
2023-03-15 15:28:08 +0400

    blazor editor

---
+++ CS/EFCore/CheckedListEF/CheckedListEF.Blazor.Server/Editors/CheckedListBoxEditor/CheckedListBoxEditor.cs
@@ -0,0 +1,47 @@
+﻿using DevExpress.ExpressApp.Blazor.Editors.Adapters;
+using DevExpress.ExpressApp.Blazor.Editors;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Model;
+using DevExpress.ExpressApp;
+using CheckedListEF.Module;
+using DevExpress.Persistent.BaseImpl;
+
+namespace CheckedListEF.Blazor.Server.Editors.CheckedListBoxEditor {
+
+    [PropertyEditor(typeof(IList<Detail>), false)]
+    public class CheckedListBoxEditor : BlazorPropertyEditorBase, IComplexViewItem {
+        public CheckedListBoxEditor(Type objectType, IModelMemberViewItem model) : base(objectType, model) { }
+        protected override IComponentAdapter CreateComponentAdapter() {
+            this.AllowEdit.Clear();
+            var allDetails = objectSpace.GetObjects<Detail>();
+            return new CheckedListBoxAdapter(new CheckedListBoxModel(allDetails));
+        }
+
+        protected override void WriteValueCore() {
+            var propertyColl = (IList<Detail>)PropertyValue;
+            var controlColl = (IEnumerable<Detail>)ControlValue;
+            foreach (var it in propertyColl) {
+                var cnt = controlColl.Where(x => x.ID == it.ID).Count();
+                if (cnt == 0) {
+                    propertyColl.Remove(it);
+                }
+            }
+            foreach (var item in controlColl) {
+                var cnt = propertyColl.Where(x => x.ID == item.ID).Count();
+                if (cnt == 0) {
+                    propertyColl.Add(item);
+                }
+            }
+            objectSpace.SetModified(CurrentObject);
+        }
+        IObjectSpace objectSpace;
+
+        #region IComplexPropertyEditor Members
+
+        public void Setup(IObjectSpace objectSpace, XafApplication application) {
+            this.objectSpace = objectSpace;
+        }
+
+        #endregion
+    }
+}
---
+++ CS/EFCore/CheckedListEF/CheckedListEF.Blazor.Server/Editors/CheckedListBoxEditor/CheckedListBoxModel.cs
@@ -0,0 +1,44 @@
+﻿using CheckedListEF.Module;
+using DevExpress.ExpressApp.Blazor.Components.Models;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Xpo;
+
+namespace CheckedListEF.Blazor.Server.Editors.CheckedListBoxEditor {
+
+    public class CheckedListBoxModel : ComponentModelBase {
+
+        public CheckedListBoxModel(IList<Detail> _details) {
+            DataSource = _details;
+        }
+        //public List<Detail> Value {
+        //    get => GetPropertyValue<List<Detail>>();
+        //    set => SetPropertyValue(value);
+        //}
+        //IEnumerable<Detail> _values;
+        //public IEnumerable<Detail> Values {
+        //    get { return _values; }
+        //    set {
+        //        _values = value.ToList();
+        //    SetValueFromUI(value);
+        //    }
+        //}
+        public IEnumerable<Detail> Values {
+            get => GetPropertyValue<IEnumerable<Detail>>();
+            set { SetPropertyValue(value); 
+                  SetValueFromUI(value);
+            }
+
+        }
+        public IList<Detail> DataSource { get; set; }
+        public bool ReadOnly {
+            get => GetPropertyValue<bool>();
+            set => SetPropertyValue(value);
+        }
+        // ...
+        public void SetValueFromUI(IEnumerable<Detail> value) {
+            SetPropertyValue(value, notify: false, nameof(Values));
+            ValueChanged?.Invoke(this, EventArgs.Empty);
+        }
+        public event EventHandler ValueChanged;
+    }
+}
